d5ffe48cb3e62dd2cf7f14bbac63c012844606de
guelmamihiba <guelmamihiba1@gmail.com>
2024-01-25 12:52:39 +0100

    done

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Home from './components/booking/home';
 import  Homeusers from './components/users/homeusers';
-import DeleteUser from './components/users/deleteusers';
 function App() {
   return (
     <div className="App">
       <Home/>
       <Homeusers/>
-      <DeleteUser/>
+      {/* <DeleteUser/> */}
 
 
 </div>
